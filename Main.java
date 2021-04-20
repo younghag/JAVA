cecbecf54ef9069f8de1f593b3bcf37ab82c4790
younghag <78770231+younghag@users.noreply.github.com>
2021-04-20 17:37:55 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,188 @@
+import java.util.*;
+class Main {
+    public static void main (String args[]) {
+        // Scanner scanner = new Scanner(System.in);
+        // char ch = ' ';
+        // System.out.printf("문자를 하나 입력하세요.>");
+        // String input = scanner.nextLine();
+        // ch = input.charAt(0);
+
+        // if('0'<=ch && ch <= '9') {
+        //     System.out.printf("입력하신 문자는 숫자입니다. %n");
+        // }
+        // if(('a'<= ch && ch<='z')||('A'<= ch && ch <= 'Z')) {
+        //     System.out.printf("입력하신 문자는 영문자 입니다. %n");
+      // int x = 0;
+      // System.out.printf("x=%d 일 때, 참인 것은 %n", x);
+      // if(x==0) System.out.println("x==0");
+      // if(x!=0) System.out.println("x!=0");
+      // if(!(x==0)) System.out.println("!(x==0)");
+      // if(!(x!=0)) System.out.println("!(x!=0)");
+
+      // x = 1;
+      // System.out.printf("x=%d 일때, 참인것은 %n", x);
+      // if(x==0) System.out.println("x==0");
+      // if(x!=0) System.out.println("x!=0");
+      // if(!(x==0)) System.out.println("x!=0");
+      // if(!(x!=0)) System.out.println("!(x!=0)");
+      // int input;
+      // System.out.print("숫자를 하나 입력하세요.>");
+      // Scanner scanner = new Scanner(System.in);
+      // String tmp = scanner.nextLine();
+      // input = Integer.parseInt(tmp);
+      // if(input==0) {
+      //   System.out.println("입력하신 숫자는 0입니다.");
+      // }
+      // if(input!=0)
+      //   System.out.println("입력하신 숫자는 0이 아닙니다.");
+      // //   System.out.printf("입력하신 숫자는 %d입니다.", input);
+      // System.out.print("숫자를 하나 입력하세요.>");
+      // Scanner scanner = new Scanner(System.in);
+      // int input = scanner.nextInt();
+      // if(input==0)
+      // int score = 0;
+      // char grade = ' ';
+      // System.out.print("점수를 입력하세요,>");
+      // Scanner scanner = new Scanner(System.in);
+      // score = scanner.nextInt();
+      // if (score >= 90) {
+      //   grade = 'A';
+      // } else if (score >= 80) {
+      //     grade = 'B';
+      // } else if (score >= 70) {
+      //     grade = 'C';
+      // } else {
+      //     grade = 'D';
+      // }
+      //   System.out.println("당신의 학점은 "+ grade + "입니다.");
+      // int score = 0;
+      // char grade = ' ', opt = '0';
+      // System.out.print("점수를 입력해주세요.>");
+      // Scanner = scanner.nextInt();
+      // System.out.printf("당신의 점수는 %d입니다. %n", score);
+      // if (score >= 90){
+      //   grade = 'A';
+      //   if (score >= 98) {
+      //     opt = '+';
+      //   } else if (score < 94) {
+      //     opt = '-';
+      //   }
+      // } else if (score >= 80) {
+      //   grade = 'B';
+      //   if(score>=88) {
+      //     opt = '+';
+      //   } else if (score < 84) {
+      //     opt = '-';
+      //   }
+      // }else{
+      //   grade = 'C';
+      // }
+      // System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
+      // System.out.print("현재 월을 입력하세요.>");
+      // int month = scanner.nextInt();
+
+      // switch(month){
+      //   case 3:
+      //   case 4:
+      //   case 5:
+      //     System.out.println("현재의 계절은 봄입니다.");
+      //     break;
+      //   case 6: case 7: case 8:
+      //     System.out.println("현재의 계절은 여름입니다.");
+      //     break;
+      //   case 9: case 10: case 11:
+      //     System.out.println("현재의 계절은 가을입니다.");
+      //     break;
+      //   default:
+      //   case 12: case 1: case 2:
+      //     System.out.println("현재의 계절은 겨울입니다.");
+      // }
+      // System.out.print("가위(1),바위(2),보(3) 중 하나를 입력하세요.>");
+      // Scanner scanner = new Scanner(System.in);
+
+      // int user = scanner.nextInt();
+      // int com = (int) (Math.random()*3+1);
+      // System.out.println("당신은 "+user + "입니다.");
+      // System.out.println("컴은 "+com+"입니다.");
+      // switch(user-com){
+      //   case 2: case -1:
+      //     System.out.println("당신이 졌습니다.");
+      //   case
+      // int sum = 0;
+      // for(int i=1; i<= 10; i++) {
+      //   sum += i;
+      //   System.out.printf("1부터 %2d 까지의 합: %2d%n", i, sum);
+    // for(int i=1; i<=5; i++){
+    //   for(int j=1; j<=i; j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+    // int[] arr = {10,20,30,40,50};
+    // int sum = 0;
+
+    // for(int i=0; i<arr.length; i++)
+    //   System.out.printf("%d ", arr[i]);
+    // System.out.println();
+    // for(int tmp : arr) {
+    //   System.out.printf("%d ", tmp);
+    //   sum += tmp;
+    // }
+    // System.out.println();
+    // System.out.println("sum="+sum);
+    // while(i--!=0) {
+    //   System.out.println(i);
+    //   for(int j=0; j<2_00_000_000;j++) {
+    //     ;
+    //   }
+    // }
+    // System.out.println("GAME OVER");
+    // int num;
+    // int sum = 0;
+    // boolean flag = true;
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("합계를 구할 숫자를 입력하세요.(끝내려면 0을 입력)");
+    // while(flag) {  
+    //   System.out.print(">>");
+    //   String tmp = scanner.nextLine();
+    //   num = Integer.parseInt(tmp);
+    //   if(num!=0){
+    //     sum += num;
+    //   } else {
+    //     flag = false;
+    //   }
+    // }
+    // // System.out.println("합계:"+sum);
+    // int input = 0, answer = 0;
+
+    // answer = (int) (Math.random() * 100) + 1;
+    // Scanner scanner = new Scanner(System.in);
+    // do {
+    //   System.out.print("1과 100사이의 정수를 입력하세요.>");
+    //   input = scanner.nextInt();
+
+    //   if(input > answer) {
+    //     System.out.println("더 작은 수로 다시 시도해 보세요.");
+    //   } else if (input < answer) {
+    //     System.out.println("더 큰 수로 다시 시도해보세요.");
+    //   }
+    // } 조ㅑㅣㄷ(ㅑㅞㅕㅅ!=)
+    // for(int i=1; i<=100; i++) {
+    //   System.out.printf("i=%d ", i);
+    //   int tmp = i;
+    //   do {
+    //     if(tmp%10%3==0 && tmp%10!=0)
+    //       System.out.print("짝");
+    //   } while((tmp/=10)!=0);
+    //   System.out.println();
+    // }
+    int menu = 0;
+    int num = 0;
+    Scanner scanner = new Scanner(System.in);
+      while(true){
+        System.out.println("(1) square");
+        System.out.println("(2) square root");
+        System.out.println("(3) log");
+      }
+    }
+}
